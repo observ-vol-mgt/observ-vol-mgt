63bfd73b495fede2e4c0d5dc84020cefe905eccf
Priyanka <priyanka.naik@ibm.com>
2024-03-22 18:33:51 +0530

    split selector

--- processor/pkg/translator/translator.go
+++ processor/pkg/translator/translator.go
@@ -1,5 +1,5 @@
-package main
-//package translator
+//package main
+package translator
 
 /***
 
@@ -17,18 +17,17 @@ import (
         //"github.com/gorilla/mux"
         "github.com/sirupsen/logrus"
         "gopkg.in/yaml.v2"
-        //morpher "github.ibm.com/dynamic-freq/PMF/processor/pkg/morpher"
-        morpher "PMF/processor/pkg/morpher"
+        morpher "github.ibm.com/dynamic-freq/PMF/processor/pkg/morpher"
 )
 
-var data = `
+/*var data = `
 processors:
   - type: filter
     metrics:
       metric_name: cluster_cpu_usage
       condition: cluster_id='wec1' and namespace_name='data-science'
       action: include/exclude
-`
+`*/
 var log = logrus.New()
 
 type DAGNode struct{
@@ -36,6 +35,7 @@ type DAGNode struct{
 }
 type Transformer struct{
    m morpher.MorphUnit 
+   s []morpher.Selector
    Type string `yaml:"type"`
    Metric struct{
    MetricName string `yaml:"metrics:metric_name"`
@@ -47,16 +47,18 @@ type Parameter struct{
    Key string
    Value string
 }
-var count = 0
 func (t Transformer) ParseSelector(){
       log.Println("Inparse")
       res1 := strings.Split(t.Metric.Condition, "and")
       for i := 0; i < len(res1); i++ {
-           res2 := strings.Split(t.Metric.Condition, "=")
-           t.m.SelectorArray[count].Key=res2[0]
-           t.m.SelectorArray[count].Value=res2[1]
-           count = count+1
+           res2 := strings.Split(res1[i], "=")
+           var s1 morpher.Selector
+           s1.Key= res2[0]
+           s1.Value = res2[1]
+           t.s = append(t.s,s1)
+           log.Println(t.s[i])
       }
+      
 }
 func (t Transformer) FillMorph(){
       log.Println("Infill")
@@ -64,15 +66,12 @@ func (t Transformer) FillMorph(){
       t.ParseSelector()
 } 
 func main() {
-    //result := make(map[string]Transformer)
-    //result := Transformer{}
     result := DAGNode{}
     content, err := ioutil.ReadFile("../../../controller/dag_examples/filter.yaml")
     if err != nil {
         log.Fatal(err.Error())
         return
     }
-    //err := yaml.Unmarshal([]byte(data), &result)
     err = yaml.Unmarshal(content, &result)
     if err != nil {
         log.Fatal("Failed to parse file ", err)
