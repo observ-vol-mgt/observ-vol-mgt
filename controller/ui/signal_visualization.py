5b4be665e64d5588478a6c480bb99e0b7e77c1ea
Eran Raichstein <eranra@il.ibm.com>
2024-04-16 17:52:28 +0300

    fix UI

--- controller/ui/signal_visualization.py
+++ controller/ui/signal_visualization.py
@@ -34,12 +34,15 @@ def visualize():
     plt.figure(figsize=(10, 6))
 
     # Plot selected time series
+    all_timestamps = []
     for series_name in selected_series:
         if series_name in time_series:
             time_stamps, data = zip(*time_series[series_name])
+            data = [float(d) for d in data]
+            time_stamps, data = zip(*sorted(zip(time_stamps, data), key=lambda x: x[1]))
+            all_timestamps.extend(time_stamps)
             time_values = [datetime.fromtimestamp(time_stamp) for time_stamp in time_stamps]
-            plt.plot(time_values, data, marker='o', label=series_name)
-
+            plt.scatter(time_values, data, label=series_name)
         else:
             return "Error: Selected time series not found."
 
@@ -50,6 +53,15 @@ def visualize():
     plt.xticks(rotation=45, fontsize=6)
     plt.grid(True)
     plt.legend()
+
+    all_timestamps.sort()  # Sort timestamps in ascending order
+    num_ticks = min(10, len(all_timestamps))  # Choose minimum of 10 or the number of timestamps
+    step = max(1, len(all_timestamps) // num_ticks)  # Calculate step size for selecting timestamps
+    selected_timestamps = all_timestamps[::step]  # Select timestamps at specified intervals
+    tick_labels = [datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') for ts in
+                   selected_timestamps]  # Format tick labels
+    plt.xticks([datetime.fromtimestamp(ts) for ts in selected_timestamps], tick_labels, rotation=45, fontsize=6)
+
     plt.savefig(f"{current_path}/temp/plot.png", dpi=120, bbox_inches='tight')  # Save the plot as an image
     plt.close()
 
