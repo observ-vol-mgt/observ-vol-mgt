475dd20dba03a7a2c59b66c9c73af69fcbda772c
Priyanka <priyanka.naik@ibm.com>
2024-04-17 23:17:49 +0530

    wrapper to add alerts dynamically to thanos ruler v1

--- manager/alertmanager/README.md
+++ manager/alertmanager/README.md
@@ -8,3 +8,18 @@
 - To note intevals and wait\_time need to be updated as per notification requirement.
 - To dynamically update rules you need to update the file and trigger reload of it in thanos ruler. e.g. using curl: `curl --request POST 0.0.0.0:10903/-/reload` (`http-address` of ruler)
 - UI: `0.0.0.0:10903` to view triggered alerts
+
+## APIs for rules
+- /add/\<rule\_id\> Add alert rule (if the rule id entry already exists it will replace it)
+- /delete/\<rule\_id\> Delete alert rule
+- /\<rule\_id\> Get details of an alert rule
+- /rules Get details of all the rules
+
+## Example CURLs
+- Add rule abc `curl -X POST --data-binary @request.yml -H "Content-type: text/x-yaml" 0.0.0.0:8090/add/abc`
+- Delete rule abc `curl  0.0.0.0:8090/delete/abc`
+- Get details of rule abc `curl  0.0.0.0:8090/abc`
+- Get details of all the rules `curl  0.0.0.0:8090/rules`
+
+TODO: Use swagger for the above documentation
+
---
+++ manager/alertmanager/thanos-ruler-endpoint.py
@@ -0,0 +1,112 @@
+from flask import Flask, jsonify, request, abort
+from config import *
+import requests
+import logging
+import yaml
+import os
+
+app = Flask(__name__)
+
+log_dir = os.path.dirname(f'{LOG_FILE}')
+logging.basicConfig(filename=f'{LOG_FILE}', filemode='w', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@app.route('/add/<rule_id>', methods=['POST'])
+def create_rule(rule_id):
+    logger.debug(f"ADD alert rule {rule_id}")
+    try:
+        rule_data = request.get_data()
+        try:
+            r_data = yaml.safe_load(rule_data)
+        except Exception as e:
+            logger.error("Invalid yaml data")
+            return {"message":"Invalid YAML data"}, 400
+        description = r_data.get('description')
+        if 'description' not in r_data.keys():
+            description = r_data.get('expr')
+        #TODO: check if rule_id already exists
+        
+        new_yaml_data_dict = {
+            'alert': f'{rule_id}',
+            'annotations': {'description': description,
+                'summary': 'Usage {{ $labels.label_0 }} of tenant {{ $labels.tenant_id }} is above 100.',
+            },
+            'expr': r_data.get('expr'),
+            'for': r_data.get('for'),
+            'labels': {'severity': 'critical'},
+        }
+        file_path = os.path.join(f"{RULES_FOLDER}", f"rules.yml")
+        with open(file_path,'r') as yamlfile:
+            cur_yaml = yaml.safe_load(yamlfile)
+            for index in range(len(cur_yaml["groups"][0]["rules"])):
+                if cur_yaml["groups"][0]["rules"][index]["alert"] == f'{rule_id}':
+                    cur_yaml["groups"][0]["rules"][index] = new_yaml_data_dict
+                    exists = True
+                    break
+            if exists != True:    
+                cur_yaml["groups"][0]["rules"].append(new_yaml_data_dict)
+        if cur_yaml:
+            with open(file_path,'w') as yamlfile:
+                yaml.safe_dump(cur_yaml, yamlfile, default_flow_style=False, sort_keys=False) 
+        url = THANOS_RULE_URL + "/-/reload"
+        r = requests.post(url)
+        logger.info(f"POST request successful for rules")
+        return {"message":"success"}, 200
+
+    except Exception as e:
+        logger.error(f"Error occurred while creating rule: {str(e)}")
+        abort(500, "An error occurred while creating the rule")
+
+@app.route('/delete/<rule_id>', methods=['GET'])
+def delete_rule(rule_id):
+    logger.debug(f"DELETE alert rule {rule_id}")
+    try:
+        file_path = os.path.join(f"{RULES_FOLDER}", f"rules.yml")
+        with open(file_path,'r') as yamlfile:
+                cur_yaml = yaml.safe_load(yamlfile)
+        for alert in cur_yaml["groups"][0]["rules"]:
+                if alert.get("alert") == f'{rule_id}':
+                    cur_yaml["groups"][0]["rules"].remove(alert)
+                    with open(file_path,'w') as yamlfile:
+                        yaml.safe_dump(cur_yaml, yamlfile, default_flow_style=False, sort_keys=False)
+                    url = THANOS_RULE_URL + "/-/reload"
+                    r = requests.post(url)
+                    logger.info(f"DELETE request successful for rules")
+                    return {"message":"success"}, 200  
+        return {"message":"success"}, 200
+    except Exception as e:
+        logger.error(f"Error occurred while deleting rule: {str(e)}")
+        abort(500, "An error occurred while deleting the rule")
+
+@app.route('/<rule_id>', methods=['GET'])
+def get_rule(rule_id):
+    logger.debug(f"GET alert rule {rule_id}")
+    try:
+        file_path = os.path.join(f"{RULES_FOLDER}", f"rules.yml")
+        with open(file_path,'r') as yamlfile:
+                cur_yaml = yaml.safe_load(yamlfile)
+        for alert in cur_yaml["groups"][0]["rules"]:
+                if alert.get("alert") == f'{rule_id}':
+                    logger.info(f"GET request successful for the rule {rule_id}")
+                    return jsonify(alert), 200
+        return {"message":"rule not found"}, 200
+    except Exception as e:
+        logger.error(f"Error occurred while getting rule: {str(e)}")
+        abort(500, "An error occurred while getting the rule")
+
+@app.route('/rules', methods=['GET'])
+def get_all_rules():
+    try:
+        rule_list = []
+        file_path = os.path.join(f"{RULES_FOLDER}", f"rules.yml")
+        with open(file_path,'r') as yamlfile:
+                cur_yaml = yaml.safe_load(yamlfile) # Note the safe_load
+        for alert in cur_yaml["groups"][0]["rules"]:
+             rule_list.append(alert)
+        logger.debug(f"GET all rules successful")
+        return jsonify(rule_list), 200
+    except Exception as e:
+        logger.error(f"Error occurred while fetching rules: {str(e)}")
+        abort(500, "An error occurred while fetching the rules")
+if __name__ == '__main__':
+    app.run(host=f"{HOST}", port=f"{PORT}", debug=True)
