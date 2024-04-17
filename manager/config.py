12b5c591673211af984ea04213c2e60cb3906449
Seep Goel <sgoel219@in.ibm.com>
2024-04-17 22:18:01 +0530

    added code for interaction with individual processors

--- manager/config.py
+++ manager/config.py
@@ -4,7 +4,8 @@ PORT = 5000
 LOG_FILE = "./app/logs/manager.log"
 PROCESSORS_FOLDER = "./app/processors"
 
-CREATE_SUFFIX = "/morphchain/createi"
+PROCESSOR_CREATE_SUFFIX = "/morphchain/create"
+PROCESSOR_DELETE_SUFFIX = "/morphchain/delete"
 
 PROCESSOR_1_URL = "http://localhost:5000"
 PROCESSOR_2_URL = "http://localhost:8100"
--- manager/main.py
+++ manager/main.py
@@ -5,6 +5,7 @@ from config import *
 import sys
 import os
 import yaml
+import json
 
 app = Flask(__name__)
 
@@ -24,7 +25,13 @@ processor_ids = []
 for key, value in processor_urls.items():
     file_name = f"{key}".split('_URL')[0].split('PROCESSOR_')[1]
     file_path = f"{PROCESSORS_FOLDER}/{file_name}.yaml"
-    processor_ids.append(file_name)
+
+    if file_name not in processor_ids:
+        processor_ids.append(file_name)
+    else:
+        logger.error(f"Duplicate URL provided for processor with id {processor_id}, we will be using the previously specified URL")
+        continue
+    
     if not os.path.exists(file_path):
         with open(file_path, 'w') as file:
                 pass  # Do nothing, file created and closed immediately
@@ -73,7 +80,7 @@ def create_processor(processor_id):
 
         # Validate if the provided data is valid YAML
         try:
-            yaml.safe_load(processor_data)
+            processor_data_yaml = yaml.safe_load(processor_data)
         except Exception as e:
             logger.error("Invalid yaml data")
             return {"message":"Invalid YAML data"}, 400
@@ -83,6 +90,16 @@ def create_processor(processor_id):
             return {"message":f"Processor with id {processor_id} not found"}, 404
         if os.path.getsize(file_path) > 0:
             logger.warning(f"File with id {processor_id} already had some content. It will be replaced.")
+
+        # ------------------------------------------------------------------------------
+        # communicate the new dag to the edge processor
+        response = requests.post(processor_urls["PROCESSOR_"+processor_id+"_URL"]+PROCESSOR_CREATE_SUFFIX, processor_data_yaml)
+        status_code = response.status_code
+        if status_code not in [200, 201]:
+            logger.error(f"Create DAG request for processor with id {processor_id} failed", response)
+            return response.json(), status_code
+        # ------------------------------------------------------------------------------
+
 
         with open(file_path, 'w') as file:
             file.write(processor_data)
@@ -98,28 +115,46 @@ def create_processor(processor_id):
 @app.route('/processors', methods=['POST'])
 def create_all_processors():
     logger.debug("CREATE request received for all processors")
+    success_list = []
     try:
         processor_data = request.get_data(as_text=True)
 
         # Validate if the provided data is valid YAML
         try:
-            yaml.safe_load(processor_data)
+            processor_data_yaml = yaml.safe_load(processor_data)
         except Exception as e:
             logger.error("Invalid yaml data")
-            return {"message":"Invalid YAML data"}, 400
+            return {"message":"Invalid YAML data", "successfully_created": success_list}, 400
 
         for processor_id in processor_ids:
             file_name = processor_id + ".yaml"
             file_path = os.path.join(PROCESSORS_FOLDER, file_name)
+
+            if not os.path.exists(file_path):
+                return {"message":f"Processor with id {processor_id} not found", "successfully_created": success_list}, 404
+            if os.path.getsize(file_path) > 0:
+                logger.warning(f"File with id {processor_id} already had some content. It will be replaced.")
+
+            # ------------------------------------------------------------------------------
+            # communicate the new dag to the edge processor
+            response = requests.post(processor_urls["PROCESSOR_"+processor_id+"_URL"]+PROCESSOR_CREATE_SUFFIX, processor_data_yaml)
+            status_code = response.status_code
+            if status_code not in [200, 201]:
+                logger.error(f"Create DAG request for processor with id {processor_id} failed", response)
+                return {"message" : json.dumps(response.json()), "successfully_created": success_list}, status_code
+            # ------------------------------------------------------------------------------
+
             with open(file_path, 'w') as file:
                 file.write(processor_data)
 
-        logger.info("POST request successful for all processors")
-        return jsonify({"message": "All processors created successfully"}), 201
+            success_list.append(processor_id)
+
+        logger.info("POST request successful for processors : ", success_list)
+        return jsonify({"message": "Create All request completed", "successfully_created": success_list}), 201
 
     except Exception as e:
         logger.error(f"Error occurred while creating all processors: {str(e)}")
-        abort(500, "An error occurred while creating all processors")
+        return {"message" : "An error occurred while creating all processors", "successfully_created": success_list}, 500
 
 
 @app.route('/processors/<processor_id>', methods=['DELETE'])
@@ -130,6 +165,17 @@ def delete_processor(processor_id):
 
         if not os.path.exists(file_path):
             return {"message":f"Processor with id {processor_id} not found"}, 404
+        
+
+        # ------------------------------------------------------------------------------
+        # delete the dag to the edge processor
+        response = requests.delete(processor_urls["PROCESSOR_"+processor_id+"_URL"]+PROCESSOR_DELETE_SUFFIX)
+        status_code = response.status_code
+        if status_code not in [200, 202, 204]:
+            logger.error(f"Delete DAG request for processor with id {processor_id} failed", response)
+            return response.json(), status_code
+        # ------------------------------------------------------------------------------
+
 
         # Replace the content of the YAML file with an empty YAML content
         with open(file_path, 'w') as file:
@@ -146,20 +192,36 @@ def delete_processor(processor_id):
 @app.route('/processors', methods=['DELETE'])
 def delete_all_processors():
     logger.debug("DELETE request received for all processors")
+
+    success_list = []
     try:
         # Iterate over all processor YAML files and replace their content with empty YAML content
         for processor_id in processor_ids:
             file_name = processor_id + ".yaml"
             file_path = os.path.join(PROCESSORS_FOLDER, file_name)
+            if not os.path.exists(file_path):
+                return {"message":f"Processor with id {processor_id} not found", "successfully_deleted" : success_list}, 404
+        
+            # ------------------------------------------------------------------------------
+            # delete the dag to the edge processor
+            response = requests.delete(processor_urls["PROCESSOR_"+processor_id+"_URL"]+PROCESSOR_DELETE_SUFFIX)
+            status_code = response.status_code
+            if status_code not in [200, 202, 204]:
+                logger.debug(f"Delete DAG request for processor with id {processor_id} failed", response)
+                return {"message" : json.dumps(response.json()), "successfully_deleted" : success_list}, status_code
+            # ------------------------------------------------------------------------------
+
             with open(file_path, 'w') as file:
                 file.write("")
 
-        logger.info("DELETE request successful for all processors")
-        return {"message": "All processors deleted successfully"}, 200
+            success_list.append(processor_id)
+
+        logger.info("DELETE request successful for processors : ", success_list)
+        return {"message": "All processors deleted successfully", "successfully_deleted" : success_list}, 200
 
     except Exception as e:
         logger.error(f"Error occurred while deleting processors: {str(e)}")
-        abort(500, "An error occurred while deleting the processors")
+        return {"message":"An error occurred while deleting the processors", "successfully_deleted" : success_list}, 500
 
 if __name__ == '__main__':
     app.run(host=f"{HOST}", port=f"{PORT}", debug=True)
