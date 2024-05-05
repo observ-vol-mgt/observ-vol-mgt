04d0041dc369fdd922695d5408e1870591f27abf
Seep Goel <sgoel219@in.ibm.com>
2024-05-05 16:55:56 +0530

    call delete processor endpoint instead of create with empty dag

--- manager/config.py
+++ manager/config.py
@@ -6,7 +6,7 @@ LOG_FILE = "./app/logs/manager.log"
 PROCESSORS_FOLDER = "./app/processors"
 RULES_FOLDER = "./app/rules"
 
-PROCESSOR_1_URL = "http://localhost:8100/morphchain/create"
-PROCESSOR_2_URL = "http://localhost:8100/morphchain/create"
+PROCESSOR_1_URL = "http://localhost:8100/morphchain"
+PROCESSOR_2_URL = "http://localhost:8100/morphchain"
 
 ALERTMANAGER_URL = "http://localhost:8090"
--- manager/utils/communication/processors.py
+++ manager/utils/communication/processors.py
@@ -7,11 +7,11 @@ logger = logging.getLogger(__name__)
 
 def add_processor(processor_url, processor_json):
     processor_yaml = yaml.dump(processor_json)
-    response = requests.post(processor_url, processor_yaml)
+    response = requests.post(processor_url+"/create", processor_yaml)
     return response
 
 def remove_processor(processor_url):
     empty_data = {}
-    response = requests.post(processor_url, yaml.dump(empty_data))
+    response = requests.post(processor_url+"/delete")
     return response
 
