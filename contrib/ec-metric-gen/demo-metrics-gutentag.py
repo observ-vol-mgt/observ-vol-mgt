622cce295ba7772018cf2ea9e8b746e42b8f4216
Priyanka <priyanka.naik@ibm.com>
2024-05-07 11:11:59 +0530

    compose and file mod for two edge setup

--- contrib/ec-metric-gen/demo-metrics-gutentag.py
+++ contrib/ec-metric-gen/demo-metrics-gutentag.py
@@ -201,7 +201,8 @@ if __name__ == '__main__':
     parser.add_argument('--conf', dest='conf', help='Config yaml file', default='conf.yaml')
     # Additional config - overrides config.yaml
     parser.add_argument('-n', '--name', dest='name', help='Name of the cluster', default='c0')
-    parser.add_argument('-p', '--port', dest='port', help='Port to run http server', default=8000, type=int)
+    parser.add_argument('-p', '--port', dest='port', help='Port to run http server for metrics', default=8000, type=int)
+    parser.add_argument('-cp', '--clientport', dest='clientport', help='Port to run http server for reconfiguration', default=5002, type=int)
     # To create artificial metrics with as many labels as we want
     parser.add_argument('--fake', dest='fake', action='store_true')
     parser.add_argument('--nmetrics', dest='nmetrics', default=1000, type=int)
@@ -222,7 +223,7 @@ if __name__ == '__main__':
     
     # Start the prometheus server
     start_http_server(args.port)
-    threading.Thread(target=lambda:app.run(host="0.0.0.0", port=5002, debug=False)).start()
+    threading.Thread(target=lambda:app.run(host="0.0.0.0", port=args.clientport, debug=False)).start()
     set_metrics_runner(args.fake)
     
 
