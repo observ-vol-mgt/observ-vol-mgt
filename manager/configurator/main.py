7d2ac4da8ee92cb0074bfc4d77dd35a1dc1c10d0
seepgoel <seep.goyal@gmail.com>
2024-06-06 12:39:13 -0500

    one version of OpenAPI documentation for procesor_config APIs

--- manager/configurator/main.py
+++ manager/configurator/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flasgger import Swagger
 import processors
 import rules
 import alerthandler
 import logging
 import os
 import yaml
-
+from models.ProcessorsConfig import ProcessorsConfig
+from models.schemas import schemas
 
 app = Flask(__name__)
 
@@ -24,9 +26,28 @@ os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filename=app.config.get('LOG_FILE'), filemode='w', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+swagger_template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Manager API",
+        "version": "1.0",
+        "description": "API to interact with the Manager"
+    },
+    "paths": {},
+    "components": {
+        'schemas': schemas
+    }
+}
+
+
 if __name__ == '__main__':
     app.register_blueprint(processors.processor_bp, url_prefix=app.config.get('APP_URL_PREFIX'))
     app.register_blueprint(rules.rules_bp, url_prefix=app.config.get('APP_URL_PREFIX'))
     app.register_blueprint(alerthandler.alerthandler_bp)
+
+
+    #Swagger(app)
+    Swagger(app, template=swagger_template)
+
     app.run(host=app.config.get('HOST'), port=app.config.get('PORT'), debug=True)
     logger.info('Manager started successfully.')
