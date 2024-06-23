3335dad699b8d88fb6f3e5ab78bd5f989fac6270
Eran Raichstein <eranra@il.ibm.com>
2024-06-23 10:05:37 +0300

    fix offline instana data (#103)

--- controller/.mk/development.mk
+++ controller/.mk/development.mk
@@ -1,3 +1,20 @@
+
+# Check if off-line data exists, if not download from Google Drive
+OFFLINE_DATA_FOLDER := "../contrib/fetch-offline-data/instana/"
+OFFLINE_DATA_DOWNLOAD_FOLDER_NAME := "demo-eu/"
+GOOGLE_DRIVE_FOLDER_URL := "https://drive.google.com/drive/folders/1etnVorTiYDxVY7XWx_0naaUTMUvYFPXo"
+
+get_offline_data:
+	if [ ! -d "$(OFFLINE_DATA_FOLDER)$(OFFLINE_DATA_DOWNLOAD_FOLDER_NAME)" ]; then \
+		$(MAKE) download_offline_data; \
+	fi
+
+download_offline_data:
+	@echo "Directory $(OFFLINE_DATA_FOLDER) does not exist. Downloading files from Google Drive..."
+	@pip install gdown > /dev/null 2>&1
+	gdown --folder $(GOOGLE_DRIVE_FOLDER_URL) -O $(OFFLINE_DATA_FOLDER)
+	@echo "Done."
+
 ##@ Development
 .PHONY: tests
 tests: install_requirements ## Execute tests
@@ -16,5 +33,5 @@ run_dev_map_reduce: install_requirements  ## Execute the pipeline using map redu
 	python main.py -c contrib/examples/config_files/map_reduce_examples/by_name.yaml
 
 .PHONY: run_dev_instana
-run_dev_instana: install_requirements  ## Execute the pipeline using instana demo data
+run_dev_instana: install_requirements get_offline_data  ## Execute the pipeline using instana demo data
 	python main.py -c contrib/examples/config_files/map_reduce_examples/from_instana.yaml
