c3c11273a3605f658d1cc1b3fd00bb4c4ae0b5ce
Seep Goel <seep.goyal@gmail.com>
2024-10-08 21:29:42 +0530

    made changes to support version controller in quay images

--- otel-collector-proxy/.mk/ci.mk
+++ otel-collector-proxy/.mk/ci.mk
@@ -1,9 +1,11 @@
+VERSION ?= latest
+
 ##@ CI
 .PHONY: ci_push
-ci_push: ## Executed upon ci push (merge) event
+ci_push: ## Executed upon ci push event
 	@echo "|||====> Executing make docker_build"
-	make docker_build
+	VERSION=$(VERSION) make docker_build
 	@echo "|||====> docker_build Done."
 	@echo "|||====> Executing make docker_push"
-	make docker_push
+	VERSION=$(VERSION) make docker_push
 	@echo "|||====> docker_push Done."
