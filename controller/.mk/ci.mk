bb0711d69ea27a90d52f587b52bee4beeb32b9d3
Seep Goel <seep.goyal@gmail.com>
2024-10-08 21:29:42 +0530

    made changes to support version controller in quay images

--- controller/.mk/ci.mk
+++ controller/.mk/ci.mk
@@ -1,24 +1,26 @@
+VERSION ?= latest
+
 ##@ CI
 .PHONY: ci_pull_request
 ci_pull_request: ## Executed upon ci pull_request event
 	@echo "|||====> Executing make Lint"
-	make lint
+	VERSION=$(VERSION) make lint
 	@echo "|||====> Lint Done."
 	@echo ""
 	@echo "|||====> Executing make docker_run_tests"
-	make docker_run_tests
+	VERSION=$(VERSION) make docker_run_tests
 	@echo "|||====> docker_run_tests Done."
 	@echo ""
 	@echo "|||====> Executing make docker_run_end2end_tests"
-	make docker_run_end2end_tests
+	VERSION=$(VERSION) make docker_run_end2end_tests
 	@echo "|||====> make docker_run_end2end_tests Done."
 	@echo ""
 
 .PHONY: ci_push
-ci_push: ci_pull_request ## Executed upon ci push (merge) event
+ci_push: ci_pull_request ## Executed upon ci push event
 	@echo "|||====> Executing make docker_build"
-	make docker_build
+	VERSION=$(VERSION) make docker_build
 	@echo "|||====> docker_build Done."
 	@echo "|||====> Executing make docker_push"
-	make docker_push
+	VERSION=$(VERSION) make docker_push
 	@echo "|||====> docker_push Done."
