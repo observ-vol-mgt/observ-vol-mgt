bfc7870c43f00ab3674de38412fe12899a61653e
Kalman Meth <meth@il.ibm.com>
2024-11-18 14:00:02 +0200

    generate configs to change freq of monotonic metics

--- controller/config_generator/config_generator_processor.py
+++ controller/config_generator/config_generator_processor.py
@@ -15,6 +15,7 @@
 import logging
 import os
 from string import Template
+from common.configuration_api import InsightsAnalysisChainType
 
 import requests
 from jinja2 import Environment, FileSystemLoader
@@ -25,6 +26,11 @@ logger = logging.getLogger(__name__)
 
 
 def generate(config, extracted_signals, signals_to_keep, signals_to_reduce):
+    generate_reduce(config, extracted_signals, signals_to_keep, signals_to_reduce)
+    generate_monotonic(config, extracted_signals, signals_to_keep, signals_to_reduce)
+
+
+def generate_reduce(config, extracted_signals, signals_to_keep, signals_to_reduce):
     signal_filter_template = config.signal_filter_template
     directory = config.directory
     processor_id_template = config.processor_id_template
@@ -73,6 +79,93 @@ def generate(config, extracted_signals, signals_to_keep, signals_to_reduce):
         # Write to file if directory exists in configuration
         if directory:
             response = write_to_file(
+                directory + f"/drop_{processor_id}", extracted_signals, output)
+            logger.debug(f"write_to_file returned: {response}")
+
+        # Send to processor URL if url exists in configuration
+        url = config.url
+        if url:
+            response = send_to_processor(url, output, processor_id)
+            logger.debug(f"send_to_processor returned: {response}")
+
+    return
+
+
+def generate_monotonic(config, extracted_signals, signals_to_keep, signals_to_reduce):
+    signal_filter_template = config.signal_filter_template
+    directory = config.directory
+    processor_id_template = config.processor_id_template
+    signal_name_template = config.signal_name_template
+    signal_condition_template = config.signal_condition_template
+
+    # if signal filtering template is set, filter signals
+    signals_to_adjust = signals_to_keep
+    if signal_filter_template != "":
+        signals_to_adjust = [signal_name for signal_name in signals_to_adjust if re.search(
+            signal_filter_template, signal_name)]
+
+    logger.debug("generating processor configuration using: ")
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template(
+        'config_generator/templates/processor_filter_processor_template_freq.yaml')
+    context_per_processor = {}
+
+    # building context per each of the processors with signals to drop (for the jinja template)
+    for _id, signal_name in enumerate(signals_to_adjust):
+        signal = extracted_signals.filter_by_names(signal_name)[0]
+        if "tags" not in signal.metadata.keys():
+            continue
+        if not signal.is_tagged([InsightsAnalysisChainType.INSIGHTS_ANALYSIS_MONOTONIC.value], True):
+            continue
+        signal_to_adjust = {"id": _id,
+                            "name": Template(signal_name_template).safe_substitute(signal.metadata),
+                            "condition": Template(signal_condition_template).safe_substitute(signal.metadata),
+                            "interval": '10000',
+                          }
+        processor_id = Template(
+            processor_id_template).safe_substitute(signal.metadata)
+
+        if processor_id not in context_per_processor:
+            context_per_processor[processor_id] = {"signals_to_adjust": []}
+
+        # Adding the signal to the list of signals to adjust, only if the signal is not already added to the list
+        found = False
+        for added_signal in context_per_processor[processor_id]['signals_to_adjust']:
+            if (added_signal['name'] == signal_to_adjust['name'] and
+                    added_signal['condition'] == signal_to_adjust['condition']):
+                found = True
+                break
+        if not found:
+            context_per_processor[processor_id]['signals_to_adjust'].append(
+                signal_to_adjust)
+
+        # writing and sending configuration to relevant processors based on configuration
+        for processor_id, processor_context in context_per_processor.items():
+            output = template.render(processor_context)
+
+            # Write to file if directory exists in configuration
+            if directory:
+                response = write_to_file(
+                    directory + f"/freq_{processor_id}", extracted_signals, output)
+                logger.debug(f"write_to_file returned: {response}")
+
+            # Send to processor URL if url exists in configuration
+            url = config.url
+            if url:
+                response = send_to_processor(url, output, processor_id)
+                logger.debug(f"send_to_processor returned: {response}")
+
+    return
+
+
+    # writing and sending configuration to relevant processors based on configuration
+    for processor_id, processor_context in context_per_processor.items():
+        output = template.render(processor_context)
+        print(f"processor_id = {processor_id}, output = {output}")
+
+        # Write to file if directory exists in configuration
+        if directory:
+            response = write_to_file(
                 directory + f"/{processor_id}", extracted_signals, output)
             logger.debug(f"write_to_file returned: {response}")
 
@@ -81,6 +174,7 @@ def generate(config, extracted_signals, signals_to_keep, signals_to_reduce):
         if url:
             response = send_to_processor(url, output, processor_id)
             logger.debug(f"send_to_processor returned: {response}")
+
 
     return
 
